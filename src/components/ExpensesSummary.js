1edb12fbbfb68fa600bb6dd95405e61631842d06
IgorrPopov <ginger_tulips@protonmail.com>
2020-04-28 11:19:27 +0300

    final deployment

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 
 
 export const ExpensesSummary = (props) => (
-    <div>
-        {
-            props.expensesCount ?
-            <h1>{
-                `Viewing ${
-                    props.expensesCount
-                } expenses totalling ${
-                    numeral(props.expensesTotal / 100).format('$0,0.00')
-                }`
-            }</h1> : ''
-        }
+    <div className="page-header">
+        <div className="content-container">
+            {
+                props.expensesCount ?
+                <h1 className="page-header__title">{
+                    `Viewing ${
+                        props.expensesCount
+                    } expenses totalling ${
+                        numeral(props.expensesTotal / 100).format('$0,0.00')
+                    }`
+                }</h1> : ''
+            }
+            <div className="page-header__actions">
+                    <Link className="button" to="/create">Add Expense</Link>
+            </div>
+        </div>
     </div>
 );
 
